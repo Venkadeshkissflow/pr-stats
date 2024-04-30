117c3c92ffaa420a883e368ff03f2011f90807b0
Satheesh kumar Maharajan <58605123+satheesh66@users.noreply.github.com>
2024-04-30 20:18:25 +0530

    fix : code clean up

--- client/src/app/pr-stats/(pages)/author/[id]/layout.js
+++ client/src/app/pr-stats/(pages)/author/[id]/layout.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 import { HeaderComponent } from "@/app/pr-stats/(components)";
-import { getAuthorInfo } from "@/app/pr-stats/route";
+// import { getAuthorInfo } from "@/app/pr-stats/route";
 
 export default async function AuthorInfoLayout({ children, params }) {
 	console.log(params, "params");
-	const authorInfo = await getAuthorInfo(params.id);
+	// const authorInfo = await getAuthorInfo(params.id);
 
-	const { name, avatarUrl, profileUrl } = authorInfo;
+	// const { name, avatarUrl, profileUrl } = authorInfo;
 
 	console.log(authorInfo, "params authorInfo");
 
 	return (
 		<div className="flex flex-col h-screen">
 			<HeaderComponent
-				title={name}
-				avatarUrl={avatarUrl}
-				profileUrl={profileUrl}
+				title={"name"}
+				avatarUrl={"avatarUrl"}
+				profileUrl={"profileUrl"}
 			/>
 			{children}
 		</div>
