ce6b3d4b13918b4c1896186c25c8fffa68d4d464
Venkadesh S <venkadesh@kissflow.com>
2024-05-12 22:06:17 +0530

    stats update

--- client/src/app/pr-stats/(pages)/query.js
+++ client/src/app/pr-stats/(pages)/query.js
@@ -21,10 +21,15 @@ import { db } from "@/app/firebaseConfig";
 const COLLECTION = {
 	CONTRIBUTORS: "contributors",
 	REVIEWS: "reviews",
+	STATS: "stats",
 };
 
 function getReviewedPrCollectionKey(authorId) {
 	return [db, COLLECTION.CONTRIBUTORS, authorId, COLLECTION.REVIEWS];
+}
+
+function getPrStatsCollectionKey(authorId) {
+	return [db, COLLECTION.CONTRIBUTORS, authorId, COLLECTION.STATS];
 }
 
 function getContributorsCollectionKey() {
@@ -35,18 +40,20 @@ async function getDocsList(dbInfo) {
 	return await getDocs(collection(...dbInfo));
 }
 
-async function getPrInfo(authorId) {
-	let prInfoCollectionSnapshot = await getDocsList(
-		getReviewedPrCollectionKey(authorId)
-	);
-	return prInfoCollectionSnapshot.docs[0].data();
+async function getPrStatsInfo(authorId) {
+	let collectionSnap = await getDocsList(getPrStatsCollectionKey(authorId));
+	return collectionSnap.docs[0].data();
 }
 
-function getAuthorsList(authorsDocList) {
-	return authorsDocList.map((authorInfoDocSnapshot) => {
-		let prInfo = getPrInfo(authorInfoDocSnapshot.id);
-		return { ...authorInfoDocSnapshot.data(), ...prInfo };
-	});
+async function getAuthorWithReviewedPr(docSnap) {
+	let prInfo = await getPrStatsInfo(docSnap.id);
+	let authotInfo = docSnap.data();
+	return { ...prInfo, ...authotInfo };
+}
+
+async function getAuthorsList(authorsDocList) {
+	let result = authorsDocList.map(getAuthorWithReviewedPr);
+	return Promise.all(result).then((data) => data);
 }
 
 export async function getContributorsList() {
