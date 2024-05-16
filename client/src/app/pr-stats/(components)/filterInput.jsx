3f44ab742d099f49d1f56ee81045cc53fc93c71d
Venkadesh S <venkadesh@kissflow.com>
2024-05-16 18:41:41 +0530

    theme support

--- client/src/app/pr-stats/(components)/filterInput.jsx
+++ client/src/app/pr-stats/(components)/filterInput.jsx
@@ -8,7 +8,7 @@ import { TIME_PERIOD } from "@/app/constant";
 
 export function FilterInput({ onFilter } = props) {
 	return (
-		<div className="flex w-52 min-h-8">
+		<div className="flex w-52 min-h-8 ">
 			<Select onValueChange={onFilter} defaultValue={TIME_PERIOD.DEFAULT}>
 				<SelectItem value={TIME_PERIOD.LAST_3_MONTHS}>Last 30 days</SelectItem>
 				<SelectItem value={TIME_PERIOD.LAST_6_MONTHS}>Last 60 days</SelectItem>
